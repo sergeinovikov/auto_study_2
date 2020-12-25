5e0e5a40ddf4c38dc8a8b26cd3482cd72881da95
Sergei Novikov <github@sergeynovi.ru>
2020-12-25 22:24:07 +0300

    Описание генератора строк

--- src/main/java/redmine/utils/StringGenerators.java
+++ src/main/java/redmine/utils/StringGenerators.java
@@ -1,4 +1,33 @@
 package redmine.utils;
 
+import java.util.Random;
+
 public class StringGenerators {
+    public static final String ENGLISH_UPPER = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    public static final String ENGLISH_LOWER = ENGLISH_UPPER.toLowerCase();
+    public static final String ENGLISH = ENGLISH_UPPER + ENGLISH_LOWER;
+
+    public static String randomEnglishString(int length) {
+        return randomString(length, ENGLISH);
+    }
+
+    public static String randomEnglishLowerString(int length) {
+        return randomString(length, ENGLISH_LOWER);
+    }
+
+    public static String randomEmail() {
+        return randomEnglishLowerString(8) + "@" + randomEnglishLowerString(8) + "." + randomEnglishLowerString(3);
+    }
+
+    public static String randomString(int length, String pattern) {
+        StringBuilder randomString = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            randomString.append(pattern.charAt(new Random().nextInt(pattern.length())));
+        }
+        return randomString.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(randomEmail());
+    }
 }
