780ccb2f929de0177b7ffd6fda652c49f591ff95
Sergei Novikov <github@sergeynovi.ru>
2021-01-29 20:11:54 +0300

    Глобальный рефактор кода. Улучшение кейсов

--- src/main/java/redmine/utils/CryptoGenerator.java
+++ src/main/java/redmine/utils/CryptoGenerator.java
@@ -1,8 +1,6 @@
 package redmine.utils;
 
-import com.google.common.hash.Hashing;
-
-import java.nio.charset.StandardCharsets;
+import static org.apache.commons.codec.digest.DigestUtils.*;
 
 /**
  * Класс описывающий генераторы HEX строки и зашифрованного пароля
@@ -12,17 +10,14 @@ public class CryptoGenerator {
     private static final String DIGITS = "1234567890";
     private static final String LETTERS_FOR_HEX = "abcdef";
 
-    public static String generateHEX(int lenght) {
-        return StringGenerators.randomString(32, DIGITS + LETTERS_FOR_HEX);
+    public static String generateHEX(int length) {
+        return StringGenerators.randomString(length, DIGITS + LETTERS_FOR_HEX);
     }
 
     public static String generatePassword(String salt) {
         String generatedPassword = StringGenerators.randomString(8, StringGenerators.ENGLISH_LOWER + DIGITS);
-        String transitPassword = Hashing.sha1()
-                .hashString(generatedPassword, StandardCharsets.UTF_8)
-                .toString();
-        return Hashing.sha1()
-                .hashString(salt + transitPassword, StandardCharsets.UTF_8)
-                .toString();
+        String transitPassword = sha1Hex(generatedPassword);
+
+        return sha1Hex((salt + transitPassword));
     }
 }
