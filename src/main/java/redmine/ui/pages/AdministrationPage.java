c99cec741b0ac7fdc7308de08d0339a7672dd1e9
Sergei Novikov <github@sergeynovi.ru>
2021-02-19 23:56:35 +0300

    PageObjects страницы "Администриарование"

---
+++ src/main/java/redmine/ui/pages/AdministrationPage.java
@@ -0,0 +1,17 @@
+package redmine.ui.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class AdministrationPage extends AbstractPage{
+    @FindBy(xpath = "//ul[@class='projects root']//a")
+    private List<WebElement> projectsList;
+
+    public Boolean getProjectElement(String projectName) {
+        return projectsList.stream()
+                .map(WebElement::getText)
+                .anyMatch(str -> str.equals(projectName));
+    }
+}
