0e0664388b6d1db005126e89a690bdd7b3a2ce59
Sergei Novikov <github@sergeynovi.ru>
2021-02-23 15:47:08 +0300

    Рефактор методов страницы пользователей. Методы сортируют любой передаваемый список

--- src/main/java/redmine/ui/pages/UsersPage.java
+++ src/main/java/redmine/ui/pages/UsersPage.java
@@ -14,34 +14,34 @@ public class UsersPage extends AbstractPage {
     @FindBy(xpath = "//table[@class='list users']")
     private WebElement usersTable;
     @FindBy(xpath = "//td[@class='username']")
-    private List<WebElement> usersFromTable;
+    private List<WebElement> usersLogins;
     @FindBy(xpath = "//a[@class='sort asc icon icon-sorted-desc']")
     private WebElement sortingUsersByLogin;
 
 
-    public void usersSortedByLoginAsc() {
-        List<String> usersLoginsWithDefaultSorting = usersFromTable.stream()
+    public void usersSortedAsc(List<WebElement> usersData) {
+        List<String> usersDataDefaultSorting = usersData.stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
 
-        List<String> usersSortedByLoginAsc = usersFromTable.stream()
+        List<String> usersDataSortedDesc = usersData.stream()
                 .map(WebElement::getText)
                 .sorted(Comparator.comparing(String::toString, String.CASE_INSENSITIVE_ORDER))
                 .collect(Collectors.toList());
 
-        Assert.assertEquals(usersLoginsWithDefaultSorting, usersSortedByLoginAsc);
+        Assert.assertEquals(usersDataDefaultSorting, usersDataSortedDesc);
     }
 
-    public void usersSortedByLoginDesc() {
-        List<String> usersLoginsWithDefaultSorting = usersFromTable.stream()
+    public void usersSortedDesc(List<WebElement> usersData) {
+        List<String> usersDataDefaultSorting = usersData.stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
 
-        List<String> usersSortedByLoginDesc = usersFromTable.stream()
+        List<String> usersDataSortedDesc = usersData.stream()
                 .map(WebElement::getText)
                 .sorted(Comparator.comparing(String::toString, String.CASE_INSENSITIVE_ORDER.reversed()))
                 .collect(Collectors.toList());
 
-        Assert.assertEquals(usersLoginsWithDefaultSorting, usersSortedByLoginDesc);
+        Assert.assertEquals(usersDataDefaultSorting, usersDataSortedDesc);
     }
 }
